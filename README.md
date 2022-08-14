95b1c871c92a8527f624ec021368220576c4c025
zadarmo <wx01290804@163.com>
2022-08-15 00:54:34 +0800

    feat: project init

---
+++ README.md
@@ -0,0 +1,11 @@
+# pypi-starter
+
+A starter template for developing python package.
+
+## setup.py
+
+Package information.
+
+## [package_name]
+
+The root path of the package.
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='<package_name>',
+    version='<version>',
+    description=(
+        '<short description>'
+    ),
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='<author name>',
+    author_email='<author email>',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=[],
+)
